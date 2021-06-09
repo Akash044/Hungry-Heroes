c5530676c1627c3b36d2a7ad1a7db61874f59a1c
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-06-09 21:50:55 +0600

    done

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -72,7 +72,7 @@ const NavBar = () => {
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>
-                      <span class="absolute right-110 top-4 rounded-full bg-red-600 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">{cart.length}</span>
+                      <span className="absolute right-110 top-4 rounded-full bg-red-600 w-4 h-4 top right p-0 m-0 text-white font-mono text-sm  leading-tight text-center">{cart.length}</span>
                     </button>
                     <button className="border ml-3 p-1 rounded-md">
                       <Link to="/checkout">
